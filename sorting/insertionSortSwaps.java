6fdfbd88995ec1b4ab382e6866ba9a6897f73002
utkrista <kcutkrista92@gmail.com>
2023-06-17 10:00:16 +1000

    insertion sort swaps count hackerrank

---
+++ sorting/insertionSortSwaps.java
@@ -0,0 +1,29 @@
+// hackerrank
+
+class Result {
+
+    /*
+     * Complete the 'runningTime' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static int runningTime(List<Integer> arr) {
+        // Write your code here
+        int totalSwaps = 0;
+        for (int i = 1; i < arr.size(); i++) {
+            int key = arr.get(i);
+            int j = i - 1;
+            while (j >= 0 && arr.get(j) > key) {
+                arr.set(j + 1, arr.get(j));
+                j = j - 1;
+                totalSwaps++;
+            }
+            arr.set(j + 1, key);
+        }
+        return totalSwaps;
+
+    }
+
+}
