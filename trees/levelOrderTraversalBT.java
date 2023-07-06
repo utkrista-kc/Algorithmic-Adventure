98ea3e2165bb2f285af4ac3263e7894495a3f70b
utkrista <kcutkrista92@gmail.com>
2023-07-06 10:00:13 +1000

    level order traversal binary tree hackerrank

---
+++ trees/levelOrderTraversalBT.java
@@ -0,0 +1,70 @@
+// hackerrank
+
+import java.util.*;
+import java.io.*;
+
+class Node {
+    Node left;
+    Node right;
+    int data;
+
+    Node(int data) {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+}
+
+class Solution {
+
+    /*
+     * 
+     * class Node
+     * int data;
+     * Node left;
+     * Node right;
+     */
+    public static void levelOrder(Node root) {
+        Queue<Node> nodesQueue = new LinkedList<>();
+        nodesQueue.add(root);
+        while (!nodesQueue.isEmpty()) {
+            Node currNode = nodesQueue.remove();
+            System.out.print(currNode.data + " ");
+            if (currNode.left != null) {
+                nodesQueue.add(currNode.left);
+            }
+            if (currNode.right != null) {
+                nodesQueue.add(currNode.right);
+            }
+        }
+
+    }
+
+    public static Node insert(Node root, int data) {
+        if (root == null) {
+            return new Node(data);
+        } else {
+            Node cur;
+            if (data <= root.data) {
+                cur = insert(root.left, data);
+                root.left = cur;
+            } else {
+                cur = insert(root.right, data);
+                root.right = cur;
+            }
+            return root;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int t = scan.nextInt();
+        Node root = null;
+        while (t-- > 0) {
+            int data = scan.nextInt();
+            root = insert(root, data);
+        }
+        scan.close();
+        levelOrder(root);
+    }
+}
