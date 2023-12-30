9a3fd0e75abfe7e804ed641a6f541df884a522ef
utkrista <kcutkrista92@gmail.com>
2023-12-31 10:07:29 +1100

    top K frequent elements

---
+++ arrays/TopKFreqElements_LC347.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+        Map<Integer, Integer> freqMap = new HashMap<>();
+        List<Integer>[] bucket = new List[nums.length + 1];
+        for (int num : nums) {
+            freqMap.put(num, freqMap.getOrDefault(num, 0) + 1);
+        }
+
+        for (int key : freqMap.keySet()) {
+            int freq = freqMap.get(key);
+            if (bucket[freq] == null) {
+                bucket[freq] = new ArrayList<>();
+            }
+            bucket[freq].add(key);
+        }
+        int[] result = new int[k];
+        int counter = 0;
+        for (int i = bucket.length - 1; i >= 0 && counter < k; i--) {
+            if (bucket[i] != null) {
+                for (int num : bucket[i]) {
+                    result[counter++] = num;
+                }
+            }
+        }
+        return result;
+    }
+}
