e8166e012149c902e2338bd24d547b105913b824
utkrista <kcutkrista92@gmail.com>
2024-01-08 09:42:47 +1100

    array product optimization

--- arrays/ArrayProduct_LC238.java
+++ arrays/ArrayProduct_LC238.java
@@ -22,3 +22,32 @@ class Solution {
 
     }
 }
+
+/*
+ * Time Complexity: O(n)
+ * space: O(1)
+ */
+
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int[] result = new int[nums.length];
+        result[0] = 1;
+        int productAllAfterCurrent = 1;
+
+        for (int i = 1; i < nums.length; i++) {
+            result[i] = nums[i - 1] * result[i - 1];
+        }
+
+        for (int i = nums.length - 1; i >= 0; i--) {
+            result[i] *= productAllAfterCurrent;
+            productAllAfterCurrent *= nums[i];
+        }
+
+        return result;
+
+    }
+}
+
+/*
+ * space complexity: O(1)
+ */
