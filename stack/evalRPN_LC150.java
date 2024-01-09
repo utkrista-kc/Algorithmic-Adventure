7b3827d2631fbd9e79bb0a34f8f0d98816c34e73
utkrista <kcutkrista92@gmail.com>
2024-01-10 09:01:55 +1100

    reverse posh notation

---
+++ stack/evalRPN_LC150.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int evalRPN(String[] tokens) {
+        Stack<Integer> stack = new Stack<Integer>();
+        for (int i = 0; i < tokens.length; i++) {
+            if (tokens[i].equals("+")) {
+                System.out.println("inside");
+                int temp = stack.pop() + stack.pop();
+                stack.push(temp);
+            } else if (tokens[i].equals("-")) {
+                int temp1 = stack.pop();
+                int temp2 = stack.pop();
+                stack.push(temp2 - temp1);
+            } else if (tokens[i].equals("*")) {
+                int temp1 = stack.pop();
+                int temp2 = stack.pop();
+                stack.push(temp2 * temp1);
+            } else if (tokens[i].equals("/")) {
+                int temp1 = stack.pop();
+                int temp2 = stack.pop();
+                stack.push(temp2 / temp1);
+            } else {
+                stack.push(Integer.parseInt(tokens[i]));
+            }
+        }
+        return stack.peek();
+    }
+}
+
+/*
+ * Time complexity: O(1)
+ * Space complexity: O(1)
+ */
