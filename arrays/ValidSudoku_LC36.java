5eef1393d0fcb092ea527a3c95b50040cf60114d
utkrista <kcutkrista92@gmail.com>
2024-01-09 21:18:25 +1100

    valid sudoku lc36

---
+++ arrays/ValidSudoku_LC36.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        Set<String> seen = new HashSet<>();
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                char num = board[i][j];
+                if (num != '.') {
+                    if (!seen.add(num + "at row" + i) ||
+                            !seen.add(num + "at col" + j) ||
+                            !seen.add(num + "at box" + (i / 3) + "-" + (j / 3))) {
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+}
+
+/*
+ * In this case time complexity: O(1), space complexity: O(1)
+ * In the case of varying input n, time complexity O(n^2) and space compolexity:
+ * O(n^2)
+ */
